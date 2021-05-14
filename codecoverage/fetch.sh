1412d37996b0bb8be3dae2b51ac815c0456024e4
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-14 19:43:25 +0100

    add http int cc

--- codecoverage/fetch.sh
+++ codecoverage/fetch.sh
@@ -10,3 +10,7 @@ rm temp.zip
 wget -O temp.zip https://nexus.galasa.dev/repository/jacoco/execs/dev.galasa.inttests/dev.galasa.inttests.core.local.CoreLocalJava11Ubuntu.zip
 unzip temp.zip -d target/inttests/dev.galasa.inttests
 rm temp.zip
+
+wget -O temp.zip https://nexus.galasa.dev/repository/jacoco/execs/dev.galasa.inttests/dev.galasa.inttests.http.local.HttpLocalJava11Ubuntu.zip
+unzip temp.zip -d target/inttests/dev.galasa.inttests
+rm temp.zip
